2a6b7d497289dc048ef22a7e0e1050885adbfca4
Corail31 <m.pietquin@free.fr>
2020-01-14 13:38:05 +0100

    chinese lang, fix to click the slidebar & #3

--- src/main/java/ovh/corail/woodcutter/client/gui/WoodcutterScreen.java
+++ src/main/java/ovh/corail/woodcutter/client/gui/WoodcutterScreen.java
@@ -6,6 +6,9 @@ import net.minecraft.client.audio.SimpleSound;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.ClickType;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.Slot;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.MathHelper;
@@ -21,13 +24,24 @@ import java.util.List;
 public class WoodcutterScreen extends ContainerScreen<WoodcutterContainer> {
     private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation("textures/gui/container/stonecutter.png");
     private float sliderProgress;
-    private boolean isRecipeClicked;
+    private boolean isSliderClicked;
     private int recipeIndexOffset;
     private boolean hasInput;
 
     public WoodcutterScreen(WoodcutterContainer containerIn, PlayerInventory playerInv, ITextComponent title) {
         super(containerIn, playerInv, title);
         containerIn.setInventoryUpdateListener(this::func_214145_d);
+    }
+
+    @Override
+    protected void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type) {
+        if (type == ClickType.PICKUP && slotId == 0 && mouseButton == 0 && slotIn != null && slotIn.slotNumber == 0) {
+            if (!this.playerInventory.getItemStack().isEmpty() && !slotIn.getStack().isEmpty() && !Container.areItemsAndTagsEqual(this.playerInventory.getItemStack(), slotIn.getStack())) {
+                this.sliderProgress = 0f;
+                this.recipeIndexOffset = 0;
+            }
+        }
+        super.handleMouseClick(slotIn, slotId, mouseButton, type);
     }
 
     @Override
@@ -93,7 +107,7 @@ public class WoodcutterScreen extends ContainerScreen<WoodcutterContainer> {
 
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int buttonId) {
-        this.isRecipeClicked = false;
+        this.isSliderClicked = false;
         if (this.hasInput) {
             int i = this.guiLeft + 52;
             int j = this.guiTop + 14;
@@ -111,9 +125,11 @@ public class WoodcutterScreen extends ContainerScreen<WoodcutterContainer> {
             }
 
             i = this.guiLeft + 119;
-            j = this.guiTop + 9;
+            j = this.guiTop + 14;
             if (mouseX >= (double) i && mouseX < (double) (i + 12) && mouseY >= (double) j && mouseY < (double) (j + 54)) {
-                this.isRecipeClicked = true;
+                this.isSliderClicked = true;
+                this.sliderProgress = MathHelper.clamp((float) (mouseY - j - 7.5f) / 40f, 0f, 1f);
+                this.recipeIndexOffset = (int) ((double) (this.sliderProgress * (float) getHiddenRows()) + 0.5d) * 4;
             }
         }
 
@@ -122,7 +138,7 @@ public class WoodcutterScreen extends ContainerScreen<WoodcutterContainer> {
 
     @Override
     public boolean mouseDragged(double dragX, double dragY, int buttonId, double p_mouseDragged_6_, double p_mouseDragged_8_) {
-        if (this.isRecipeClicked && canScroll()) {
+        if (this.isSliderClicked && canScroll()) {
             int i = this.guiTop + 14;
             int j = i + 54;
             this.sliderProgress = ((float) dragY - (float) i - 7.5f) / ((float) (j - i) - 15f);
