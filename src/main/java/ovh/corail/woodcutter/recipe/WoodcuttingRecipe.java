192e7a87afbbc3487b87f70268ba5147c18c5141
Corail31 <m.pietquin@free.fr>
2020-01-20 13:23:02 +0100

    lazy solution for serializer of recipe
    As fabric has no AT

--- src/main/java/ovh/corail/woodcutter/recipe/WoodcuttingRecipe.java
+++ src/main/java/ovh/corail/woodcutter/recipe/WoodcuttingRecipe.java
@@ -1,12 +1,16 @@
 package ovh.corail.woodcutter.recipe;
 
+import com.google.gson.JsonObject;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.CuttingRecipe;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.StonecuttingRecipe;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
 import net.minecraft.world.World;
 import ovh.corail.woodcutter.registry.ModBlocks;
 import ovh.corail.woodcutter.registry.ModRecipeSerializers;
@@ -27,5 +31,33 @@ public class WoodcuttingRecipe extends CuttingRecipe {
     @Environment(EnvType.CLIENT)
     public ItemStack getRecipeKindIcon() {
         return ModBlocks.createRandomStack();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static class CustomSerializer<T extends CuttingRecipe> implements RecipeSerializer<T> {
+        final RecipeFactory<T> recipeFactory;
+
+        public CustomSerializer(RecipeFactory<T> recipeFactory) {
+            this.recipeFactory = recipeFactory;
+        }
+
+        @Override
+        public T read(Identifier id, JsonObject json) {
+            return (T) STONECUTTING.read(id, json);
+        }
+
+        @Override
+        public T read(Identifier id, PacketByteBuf buf) {
+            return (T) STONECUTTING.read(id, buf);
+        }
+
+        @Override
+        public void write(PacketByteBuf buf, T recipe) {
+            STONECUTTING.write(buf, (StonecuttingRecipe) recipe);
+        }
+
+        public interface RecipeFactory<T extends CuttingRecipe> {
+            T create(Identifier identifier, String string, Ingredient ingredient, ItemStack itemStack);
+        }
     }
 }
--- src/main/java/ovh/corail/woodcutter/registry/ModRecipeSerializers.java
+++ src/main/java/ovh/corail/woodcutter/registry/ModRecipeSerializers.java
@@ -1,6 +1,5 @@
 package ovh.corail.woodcutter.registry;
 
-import net.minecraft.recipe.CuttingRecipe;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -9,7 +8,7 @@ import ovh.corail.woodcutter.recipe.WoodcuttingRecipe;
 import static ovh.corail.woodcutter.WoodCutterMod.MOD_ID;
 
 public class ModRecipeSerializers {
-    public static final RecipeSerializer<WoodcuttingRecipe> WOODCUTTING = new CuttingRecipe.Serializer<WoodcuttingRecipe>(WoodcuttingRecipe::new) {};
+    public static final RecipeSerializer<WoodcuttingRecipe> WOODCUTTING = new WoodcuttingRecipe.CustomSerializer<>(WoodcuttingRecipe::new);
 
     public static void init() {
         Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(MOD_ID, "woodcutting"), WOODCUTTING);
