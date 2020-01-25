ec601dff422d5eff4d0ce303e8cd52f57267d6ca
Corail31 <m.pietquin@free.fr>
2020-01-25 07:55:08 +0100

    use reflection to instanciate POI
    because the mixin was working in workspace but not in launcher

--- src/main/java/ovh/corail/woodcutter/registry/ModPointOfInterestTypes.java
+++ src/main/java/ovh/corail/woodcutter/registry/ModPointOfInterestTypes.java
@@ -5,15 +5,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.village.PointOfInterestType;
-import ovh.corail.woodcutter.mixin.PointOfInterestTypeMixin;
+import net.minecraft.world.poi.PointOfInterestType;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Set;
 
 import static ovh.corail.woodcutter.WoodCutterMod.MOD_ID;
 
 public class ModPointOfInterestTypes {
-    public static final PointOfInterestType WOODSMITH = PointOfInterestTypeMixin.createPointOfInterestType("woodsmith", getAllStates(), 1, 1);
+    public static Constructor<?> CONSTRUCTOR = Arrays.stream(PointOfInterestType.class.getDeclaredConstructors()).filter(constructor -> constructor.getParameterCount() == 4).findFirst().orElse(null);
+    static {
+        if (CONSTRUCTOR == null) {
+            throw new Error("Impossible to instanciate a new PointOfInterestType");
+        }
+        CONSTRUCTOR.setAccessible(true);
+    }
+    public static final PointOfInterestType WOODSMITH = createPOI();
 
     public static void init() {
         Registry.register(Registry.POINT_OF_INTEREST_TYPE, new Identifier(MOD_ID, "woodsmith"), WOODSMITH);
@@ -25,5 +34,16 @@ public class ModPointOfInterestTypes {
             builder.addAll(block.getStateManager().getStates());
         }
         return builder.build();
+    }
+
+    private static PointOfInterestType createPOI() {
+        if (CONSTRUCTOR == null) {
+            return null;
+        }
+        try {
+            return (PointOfInterestType) CONSTRUCTOR.newInstance("woodsmith", getAllStates(), 1, 1);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            return null;
+        }
     }
 }
