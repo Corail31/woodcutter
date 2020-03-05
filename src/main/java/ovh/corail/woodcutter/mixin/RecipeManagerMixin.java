543a38993af7b7409a8c898b779a612b6dd17c46
Corail31 <17954109+Corail31@users.noreply.github.com>
2020-03-05 11:19:40 +0100

    minor change

--- src/main/java/ovh/corail/woodcutter/mixin/RecipeManagerMixin.java
+++ src/main/java/ovh/corail/woodcutter/mixin/RecipeManagerMixin.java
@@ -1,5 +1,6 @@
 package ovh.corail.woodcutter.mixin;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.recipe.RecipeManager;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -29,19 +31,19 @@ public class RecipeManagerMixin {
         Map<String, Boolean> mods = new HashMap<>();
         while (it.hasNext()) {
             Map.Entry<Identifier, JsonObject> entry = it.next();
-            if (customRecipeType.equals(JsonHelper.getString(entry.getValue(), "type"))) {
-                String result = JsonHelper.getString(entry.getValue(), "result", "");
+            if (customRecipeType.equals(getString(entry.getValue(), "type"))) {
+                String result = getString(entry.getValue(), "result");
                 if (result.contains(":") && !mods.computeIfAbsent(result.split(":")[0], FabricLoader.getInstance()::isModLoaded)) {
                     it.remove();
                     LOGGER.debug(String.format("%s: disabling recipe %s", MOD_ID, entry.getKey().toString()));
                 } else {
-                    JsonObject obj = JsonHelper.hasArray(entry.getValue(), "ingredient") ? null : JsonHelper.getObject(entry.getValue(), "ingredient", null);
-                    if (obj != null && obj.isJsonObject()) {
+                    JsonObject obj = getObject(entry.getValue(), "ingredient");
+                    if (obj != null) {
                         String ingredient = "";
                         if (obj.has("tag")) {
-                            ingredient = JsonHelper.getString(obj, "tag", "");
+                            ingredient = getString(obj, "tag");
                         } else if (obj.has("item")) {
-                            ingredient = JsonHelper.getString(obj, "item", "");
+                            ingredient = getString(obj, "item");
                         }
                         if (ingredient.contains(":") && !mods.computeIfAbsent(ingredient.split(":")[0], FabricLoader.getInstance()::isModLoaded)) {
                             it.remove();
@@ -55,5 +57,16 @@ public class RecipeManagerMixin {
         if (!toDisplay.isEmpty()) {
             LOGGER.info(String.format("%s: some recipes have been disabled for the following domains : %s", MOD_ID, toDisplay));
         }
+    }
+
+    private String getString(JsonObject object, String element) {
+        JsonElement jsonElement;
+        return object.has(element) && (jsonElement = object.get(element)).isJsonPrimitive() ? jsonElement.getAsString() : "";
+    }
+
+    @Nullable
+    private JsonObject getObject(JsonObject object, String element) {
+        JsonElement jsonElement;
+        return !JsonHelper.hasArray(object, element) && object.has(element) && (jsonElement = object.get(element)).isJsonObject() ? jsonElement.getAsJsonObject() : null;
     }
 }
