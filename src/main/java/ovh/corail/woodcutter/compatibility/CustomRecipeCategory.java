6ad91526491c59ad9ba78188aafda7e7888d5aef
Corail31 <68999590+Corail31@users.noreply.github.com>
2021-06-30 13:19:10 +0200

    about Jei

--- src/main/java/ovh/corail/woodcutter/compatibility/CustomRecipeCategory.java
+++ src/main/java/ovh/corail/woodcutter/compatibility/CustomRecipeCategory.java
@@ -7,24 +7,27 @@ import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.category.IRecipeCategory;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.SingleItemRecipe;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class CustomRecipeCategory<T extends SingleItemRecipe> implements IRecipeCategory<T> {
     private static final ResourceLocation RECIPE_GUI_VANILLA = new ResourceLocation("jei", "textures/gui/gui_vanilla.png");
     private final ResourceLocation uid;
     private static final int WIDTH = 116, HEIGHT = 18;
     private final IDrawable background, icon;
-    private final String localizedName;
+    private final String translationKey;
+    private final ITextComponent translation;
     private final Class<T> tClass;
 
     CustomRecipeCategory(String translationKey, ResourceLocation uid, ItemStack icon, Class<T> tClass, IGuiHelper guiHelper) {
         this.uid = uid;
         this.background = guiHelper.drawableBuilder(RECIPE_GUI_VANILLA, 49, 168, WIDTH, HEIGHT).addPadding(0, 0, 40, 0).build();
         this.icon = guiHelper.createDrawableIngredient(icon);
-        this.localizedName = I18n.format(translationKey);
+        this.translationKey = translationKey;
+        this.translation = new TranslationTextComponent(translationKey);
         this.tClass = tClass;
     }
 
@@ -39,8 +42,13 @@ public class CustomRecipeCategory<T extends SingleItemRecipe> implements IRecipe
     }
 
     @Override
-    public String getTitle() {
-        return this.localizedName;
+    public String getTitle() { // deprecated but not default
+        return this.translationKey;
+    }
+
+    @Override
+    public ITextComponent getTitleAsTextComponent() { // default is not using translation
+        return this.translation;
     }
 
     @Override
