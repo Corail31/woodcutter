37069577273d27f86591307a006c3525162ea308
Corail31 <68999590+Corail31@users.noreply.github.com>
2022-05-14 21:27:44 +0200

    Release 2.0.2 for 1.16.5
    - fix #20 (command /cwc with some mods)
    - Korean, Russian & Catalan translations (courtesy of Gjeodnd12165, DrHesperus & VerdaPegasus)

--- src/main/java/ovh/corail/woodcutter/command/CommandWoodcutter.java
+++ src/main/java/ovh/corail/woodcutter/command/CommandWoodcutter.java
@@ -400,7 +400,7 @@ public class CommandWoodcutter {
     }
 
     private void addRecipe(final Map<String, WoodcuttingJsonRecipe> recipes, ResourceLocation input, ResourceLocation output, int count, boolean isTag) {
-        recipes.put(output.getPath() + "_from_" + input.getPath(), new WoodcuttingJsonRecipe(input.toString(), output.toString(), count, isTag));
+        recipes.put(output.getPath() + "_from_" + input.getPath().replaceAll("/|\\\\", "_"), new WoodcuttingJsonRecipe(input.toString(), output.toString(), count, isTag));
     }
 
     private void addPlankRecipe(final Map<String, WoodcuttingJsonRecipe> recipes, WoodCompo compo, ResourceLocation output, int count) {
